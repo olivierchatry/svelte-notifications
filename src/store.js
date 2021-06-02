c72d35f8249d5eded0baffae17120bc2b88a6fa0
Olivier Chatry <olivier@futureon.com>
2021-06-02 13:33:30 +0200

    trying to fix issue with vite

--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
 import { writable } from 'svelte/store'
 
-export const notification = writable()
+if (!window.__svelte_notifications_store__) {
+  window.__svelte_notifications_store__ = writable()
+}
+
+export const notification = window.__svelte_notifications_store__
 
